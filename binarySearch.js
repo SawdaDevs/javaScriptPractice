2e4352c495a9f3f1c0b8cc855f3f0cbf7bf7998c
SawdaDevs <sawda.onanuga@gmail.com>
2022-06-14 01:02:45 +0100

    corrected binarysearch

--- binarySearch.js
+++ binarySearch.js
@@ -1,41 +1,49 @@
-// """You're going to write a binary search function.
-// You should use an iterative approach - meaning
-// using loops.
-// Your function should take two inputs:
-// a Python list to search through, and the value
-// you're searching for.
-// Assume the list only has distinct elements,
-// meaning there are no repeated values, and 
-// elements are in a strictly increasing order.
-// Return the index of value, or -1 if the value
-// doesn't exist in the list."""
+const binary_search_iter= (inputArray, value) => {
 
-const binary_search = (input_array, value) => {
-
-    let index_to_go = Math.round((input_array.length) / 2); //can I do this recursively
-    let start_array = input_array.length;
-    let end_array = input_array.length;
-
-    while (input_array.slice(start_array, end_array).length > 1 && input_array[index_to_go] != value) {
-        if (value > input_array[index_to_go]) {
-            start_array = index_to_go
-            index_to_go = Math.round(start_array + end_array) / 2
-            console.log('New start: ', start_array, ' new end: ', end_array, ' new array', input_array.slice(start_array, end_array))
-        } else if (value < input_array[index_to_go]) {
-            end_array = index_to_go
-            index_to_go = Math.round(start_array + end_array) / 2
-            console.log('New start: ', start_array, ' new end: ', end_array, ' new array', input_array.slice(start_array, end_array))
+    let indexToGo = Math.round((inputArray.length) / 2);
+    let startArray = 0;
+    let endArray = inputArray.length;
+    console.log('The array we are working with is:', inputArray)
+    console.log('We are looking for the value: ', value )
+    //should check for array being ordered...how?
+    while (inputArray.slice(startArray, endArray).length > 1 && inputArray[indexToGo] != value) {
+        if (value > inputArray[indexToGo]) {
+            startArray = indexToGo
+            indexToGo = Math.round((startArray + endArray) / 2)
+            console.log('New start: ', startArray, ' new end: ', endArray, ' new array', inputArray.slice(startArray, endArray), 'Index now is:', indexToGo)
+        } else if (value < inputArray[indexToGo]) {
+            endArray = indexToGo
+            indexToGo = Math.round((startArray + endArray) / 2)
+            console.log('New start: ', startArray, ' new end: ', endArray,  ' new array', inputArray.slice(startArray, endArray), 'Index now is:', indexToGo)
         }
     }
-    if (input_array[index_to_go] == value) {
-        return index_to_go
+    if (inputArray[indexToGo] == value) {
+        return  `The number is found in the array at index : ${indexToGo}`
     };
-    return -1
+    return `Unfortunately, the number is not found in the array`
+}
+
+const binary_search_recu = (inputArray, value)=>{
+    //indexToSearch
+    //arrayStart
+    //arrayEnd
+    const binarysearch = (  , value) =>{
+        let indexToGo = Math.round((startArray + endArray) / 2)
+    }
+   
+
 }
 
 
-const test_list = [1, 3, 9, 11, 15, 19, 29]
-const test_val1 = 25
-const test_val2 = 15
-console.log(binary_search(test_list, test_val1))
-console.log(binary_search(test_list, test_val2))
+const testList = [1, 3, 9, 11, 15, 19, 29]
+const testVal1 = 29
+const testVal2 = 19
+const testVal3 = 3
+const testVal4 = 0
+console.log(binary_search_iter(testList, testVal1))
+console.log('___')
+console.log(binary_search_iter(testList, testVal2))
+console.log('___')
+console.log(binary_search_iter(testList, testVal3))
+console.log('___')
+console.log(binary_search_iter(testList, testVal4))

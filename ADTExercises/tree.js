62c5e6182fb8890393bb385464644b81073b13f9
SawdaDevs <sawda.onanuga@gmail.com>
2022-09-12 01:06:18 +0100

    Binary Search

--- ADTExercises/tree.js
+++ ADTExercises/tree.js
@@ -4,6 +4,8 @@ class Node{
         this.right = null 
         this.left = null
         //new node with null as left and right pointers
+
+        //could introduce counter, default being zero or null
     }
 }
 class BinaryTree{
@@ -11,6 +13,9 @@ class BinaryTree{
         this.root = null
     }
     insert(value){
+
+    //what to do with same value?
+
         //first create the node to be inserted
         let newNode = new Node(value)
         if(this.root === null){
@@ -42,9 +47,40 @@ class BinaryTree{
             }
         }
     }
-    //what to do with same value?
     remove(value){
         //first search
         //remove if found 
+    }
+    removeLeafNode(){
+
+    }
+    removeOneChild(){
+        
+    }
+    removeTwoChild(){
+
+    }
+    search(currentNode, value){
+        //how do I handle value not found? keep going until I find null where the value should be, if I hit null then return null or not found
+        
+        //start from root node currentNode =this.root
+
+        if (currentNode === null){
+            //binary tree is empty
+            return null || `${value} not found`
+        }else if (currentNode.value>value){
+            //go left
+            currentNode = currentNode.left
+            return this.search(currentNode, value)
+        }else if (currentNode.value<value){
+            //go left
+            currentNode = currentNode.right
+            return this.search(currentNode, value)
+        }
+        else{
+            //same value
+            return 'Found value'
+        }
+
     }
 }
